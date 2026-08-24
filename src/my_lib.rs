8dd819035213d4567d61a942a926bbea1c9651de
zvvp <zvvpopov@gmail.com>
2026-08-24 11:26:44 +0300

    Плохо работает с записями больше двух суток

--- src/my_lib.rs
+++ src/my_lib.rs
@@ -1,4 +1,6 @@
 use crate::multi_thread_func;
+use crate::time_param::TimeParam;
+use crate::zub_p::Zubp;
 use log::debug;
 use native_dialog::{DialogBuilder, MessageLevel};
 use std::fs::File;
@@ -72,6 +74,8 @@ impl Leads {
         let t_1 = thread::spawn(|| {
             ch_1 = multi_thread_func!(cut_impuls, &ch_1);
             ch_1 = multi_thread_func!(cut_impuls, &ch_1);
+            ch_1 = multi_thread_func!(clean_ch, &ch_1);
+            ch_1 = multi_thread_func!(filter_lp_35d0, &ch_1);
             ch_1 = multi_thread_func!(del_isoline, &ch_1);
             ch_1
         });
@@ -79,6 +83,8 @@ impl Leads {
         let t_2 = thread::spawn(|| {
             ch_2 = multi_thread_func!(cut_impuls, &ch_2);
             ch_2 = multi_thread_func!(cut_impuls, &ch_2);
+            ch_2 = multi_thread_func!(clean_ch, &ch_2);
+            ch_2 = multi_thread_func!(filter_lp_35d0, &ch_2);
             ch_2 = multi_thread_func!(del_isoline, &ch_2);
             ch_2
         });
@@ -86,6 +92,8 @@ impl Leads {
         let t_3 = thread::spawn(|| {
             ch_3 = multi_thread_func!(cut_impuls, &ch_3);
             ch_3 = multi_thread_func!(cut_impuls, &ch_3);
+            ch_3 = multi_thread_func!(clean_ch, &ch_3);
+            ch_3 = multi_thread_func!(filter_lp_35d0, &ch_3);
             ch_3 = multi_thread_func!(del_isoline, &ch_3);
             ch_3
         });
@@ -399,4 +407,282 @@ pub fn moving_average(data: &Vec<f32>, window_size: usize) -> Vec<f32> {
         out.push(last_out);
     }
     out
+}
+
+pub fn my_filtfilt(b: &Vec<f32>, a: &Vec<f32>, ch: &Vec<f32>) -> Vec<f32> {
+    let mut temp = ch.to_owned();
+    let mut out = ch.to_owned();
+    let len_b = b.len();
+    let len_a = a.len();
+    let len_ch = ch.len();
+
+    for i in len_b - 1..len_ch {
+        temp[i] = b[0] * ch[i];
+        for j in 1..len_b {
+            temp[i] += b[j] * ch[i - j];
+        }
+        for j in 1..len_a {
+            temp[i] -= a[j] * temp[i - j];
+        }
+    }
+
+    for i in (1..=(len_ch - len_b)).rev() {
+        out[i] = b[0] * temp[i];
+        for j in 1..len_b {
+            out[i] += b[j] * temp[i + j];
+        }
+        for j in 1..len_a {
+            out[i] -= a[j] * out[i + j];
+        }
+    }
+    out
+}
+
+pub fn get_coef_p(leads: &Leads, time_param: &TimeParam) -> Vec<f32> {
+    let r_pos_len = time_param.r_pos.len();
+    let mut zub_p1 = Zubp::new(&leads.lead1, r_pos_len);
+    let mut zub_p2 = Zubp::new(&leads.lead2, r_pos_len);
+    let mut zub_p3 = Zubp::new(&leads.lead3, r_pos_len);
+    zub_p1.get_mean_amp_pos(&leads.lead1, time_param);
+    zub_p2.get_mean_amp_pos(&leads.lead2, time_param);
+    zub_p3.get_mean_amp_pos(&leads.lead3, time_param);
+    let mut presence_pr: Vec<i32> = vec![];
+    for i in 0..zub_p1.presence_pr.len() {
+        let mut vec_pr = vec![
+            zub_p1.presence_pr[i],
+            zub_p2.presence_pr[i],
+            zub_p3.presence_pr[i],
+        ];
+        let med_pr = median(&mut vec_pr);
+        presence_pr.push(med_pr);
+    }
+    presence_pr = median_filter(&presence_pr, 59); // 19
+    zub_p1.presence_pr = presence_pr.clone();
+    zub_p2.presence_pr = presence_pr.clone();
+    zub_p3.presence_pr = presence_pr.clone();
+
+    let p1 = zub_p1.get_p_in_lead(&leads.lead1, time_param);
+    let p2 = zub_p2.get_p_in_lead(&leads.lead2, time_param);
+    let p3 = zub_p3.get_p_in_lead(&leads.lead3, time_param);
+
+    let mut out: Vec<f32> = vec![0.0; time_param.r_pos.len()];
+    for i in 4..p1.len() {
+        let mut sum_p1 = p1[i - 4] + p1[i - 3] + p1[i - 2] + p1[i - 1] + p1[i];
+        let mut sum_p2 = p2[i - 4] + p2[i - 3] + p2[i - 2] + p2[i - 1] + p2[i];
+        let mut sum_p3 = p3[i - 4] + p3[i - 3] + p3[i - 2] + p3[i - 1] + p3[i];
+
+        if ((sum_p1 < 2.5) && (sum_p2 > 2.5) && (sum_p3 > 2.5))
+            || ((sum_p1 > 2.5) && (sum_p2 < 2.5) && (sum_p3 < 2.5))
+        {
+            if sum_p1 < 5.0 {
+                sum_p1 = (sum_p2 + sum_p3) / 2.0;
+            }
+        } else if ((sum_p2 < 2.5) && (sum_p1 > 2.5) && (sum_p3 > 2.5))
+            || ((sum_p2 > 2.5) && (sum_p1 < 2.5) && (sum_p3 < 2.5))
+        {
+            if sum_p2 < 5.0 {
+                sum_p2 = (sum_p1 + sum_p3) / 2.0;
+            }
+        } else if ((sum_p3 < 2.5) && (sum_p1 > 2.5) && (sum_p2 > 2.5))
+            || ((sum_p3 > 2.5) && (sum_p1 < 2.5) && (sum_p2 < 2.5))
+        {
+            if sum_p3 < 5.0 {
+                sum_p3 = (sum_p1 + sum_p2) / 2.0;
+            }
+        }
+        let sum_buf = sum_p1 * sum_p2 * sum_p3 * 0.3; // * 0.38;
+        out[i - 2] = sum_buf;
+    }
+    let max_out: f32 = out.iter().fold(f32::MIN, |a, b| a.max(*b));
+    for i in 0..out.len() {
+        out[i] = -(out[i] - max_out);
+    }
+    let out_len = out.len();
+    out[0] = out[2];
+    out[1] = out[2];
+    out[out_len - 2] = out[out_len - 3];
+    out[out_len - 1] = out[out_len - 3];
+
+    out = step_moving_average(&out, 20);
+    out = moving_average(&out, 40);
+    out = moving_average(&out, 20);
+    out
+}
+
+pub fn get_coef_fibr(coef_p: &Vec<f32>, coef_disp: &Vec<f32>, time_param: &TimeParam) -> Vec<f32> {
+    let mut out: Vec<f32> = vec![0.0; coef_p.len()];
+    for i in 0..coef_p.len() {
+        let x = time_param.threshold[i];
+        out[i] = coef_p[i] * coef_disp[i] * ((-((x - 110.0) / 150.0).powi(2)).exp() * 0.8 + 0.65);
+    }
+    out = truncate_win2(&out, 0.85, 160);
+    out = truncate_win2(&out, 0.75, 80);
+    out
+}
+
+pub fn truncate_win2(ch: &Vec<f32>, k: f32, win_size: usize) -> Vec<f32> {
+    let mut out = ch.clone();
+    let half_win = win_size / 2;
+
+    for i in ((half_win)..(&out.len() - half_win)).step_by(half_win / 2) {
+        let slice_start = i - half_win;
+        let slice_end = i + half_win;
+        let buff = &out[slice_start..slice_end].to_vec();
+
+        let mean_buff: f32 = buff.iter().sum::<f32>() / (buff.len() as f32);
+        let over_buff: Vec<f32> = buff.iter().filter(|x| **x > mean_buff).cloned().collect();
+        if over_buff.len() > 0 {
+            let over_mean: f32 = over_buff.iter().sum::<f32>() / (over_buff.len() as f32);
+            for j in slice_start..slice_end {
+                if out[j] > over_mean {
+                    out[j] = (out[j] - over_mean) * k + over_mean;
+                }
+            }
+        }
+        let under_buff: Vec<f32> = buff.iter().filter(|x| **x < mean_buff).cloned().collect();
+        if under_buff.len() > 0 {
+            let under_mean: f32 = under_buff.iter().sum::<f32>() / (under_buff.len() as f32);
+            for j in slice_start..slice_end {
+                if out[j] < under_mean {
+                    out[j] = (out[j] - under_mean) * k + under_mean;
+                }
+            }
+        }
+    }
+    // Заполнение краевых участков
+    for i in 0..half_win {
+        out[i] = out[half_win];
+    }
+    for i in (out.len() - half_win)..out.len() {
+        out[i] = out[out.len() - half_win];
+    }
+    out
+}
+
+pub fn median(vec: &mut Vec<i32>) -> i32 {
+    // Сортируем вектор
+    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    let len = vec.len();
+    if len % 2 == 0 {
+        // Если четное количество элементов
+        let mid1 = vec[len / 2 - 1];
+        let mid2 = vec[len / 2];
+        (mid1 + mid2) / 2
+    } else {
+        // Если нечетное количество элементов
+        vec[len / 2]
+    }
+}
+
+pub fn median_filter(input: &Vec<i32>, window_size: usize) -> Vec<i32> {
+    let mut output = vec![0; input.len()]; // Инициализируем выходной вектор нулями
+    let half_window = window_size / 2;
+
+    for i in half_window..input.len() - half_window {
+        let start = i - half_window;
+        let end = i + half_window + 1;
+
+        let mut window: Vec<i32> = input[start..end].to_vec();
+        output[i] = median(&mut window);
+    }
+    for i in 0..half_window {
+        output[i] = output[half_window];
+    }
+    for i in output.len() - half_window..output.len() {
+        output[i] = output[i - half_window];
+    }
+    output
+}
+
+pub fn find_local_extrema(data: &Vec<f32>) -> (Vec<usize>, Vec<f32>) {
+    let mut vec_ind_extrema: Vec<usize> = vec![];
+    let mut vec_val_extrema: Vec<f32> = vec![];
+    for i in 1..data.len() - 1 {
+        if (data[i] > data[i - 1] && data[i] > data[i + 1])
+            || (data[i] < data[i - 1] && data[i] < data[i + 1])
+        {
+            vec_ind_extrema.push(i);
+            vec_val_extrema.push(data[i]);
+        }
+    }
+    if vec_ind_extrema.len() == 0 {
+        vec_ind_extrema.push(0);
+        vec_val_extrema.push(0.0);
+    }
+    (vec_ind_extrema, vec_val_extrema)
+}
+
+pub fn find_max(vec_max: &Vec<f32>) -> (f32, usize) {
+    /* Находит в фрагменте индекс максимального локального максимума
+    и значение макс. лок. */
+    let mut max: f32 = 0.0;
+    let mut ind: usize = 0;
+    for (i, item) in vec_max.iter().enumerate() {
+        if *item > max {
+            max = *item;
+            ind = i;
+        }
+    }
+    (max, ind)
+}
+
+pub fn filter_lp_35d0(ch: &Vec<f32>) -> Vec<f32> {
+    let b: Vec<f32> = vec![0.11735104, 0.23470207, 0.11735104];
+    let a: Vec<f32> = vec![1.0, -0.82523238, 0.29463653];
+    let out: Vec<f32> = my_filtfilt(&b, &a, &ch);
+    out
+}
+
+fn get_spec24(ch: &Vec<f32>) -> Vec<f32> {
+    let bp = vec![0.05695238, 0.0, -0.05695238];
+    let ap = vec![1.0, -1.55326091, 0.88609524];
+
+    let bl = vec![0.11216024, 0.11216024];
+    let al = vec![1.0, -0.77567951];
+
+    let bh = vec![0.97547839, -0.97547839];
+    let ah = vec![1.0, -0.95095678];
+
+    let spec24 = my_filtfilt(&bp, &ap, &ch);
+    let spec24 = spec24
+        .iter()
+        .map(|&x| (x * 4.0).abs())
+        .collect::<Vec<f32>>();
+    let spec24 = my_filtfilt(&bl, &al, &spec24);
+    let spec24 = my_filtfilt(&bh, &ah, &spec24);
+
+    spec24
+}
+
+fn get_spec50(ch: &Vec<f32>) -> Vec<f32> {
+    let bp = vec![0.13672874, 0.0, -0.13672874];
+    let ap = vec![1.0, -0.53353098, 0.72654253];
+
+    let bl = vec![0.24523728, 0.24523728];
+    let al = vec![1.0, -0.50952545];
+
+    let spec50 = my_filtfilt(&bp, &ap, &ch);
+    let spec50 = spec50.iter().map(|&x| x.abs()).collect::<Vec<f32>>();
+    let spec50 = my_filtfilt(&bl, &al, &spec50);
+
+    spec50
+}
+
+pub fn clean_ch(ch: &Vec<f32>) -> Vec<f32> {
+    // b, a = butter(2, 20, 'lp', fs=250)
+    let b = vec![0.0461318, 0.0922636, 0.0461318];
+    let a = vec![1.0, -1.30728503, 0.49181224];
+
+    let spec24 = get_spec24(&ch);
+    let spec50 = get_spec50(&ch);
+    let clean_ch = my_filtfilt(&b, &a, &ch);
+    let mut fch = ch.clone();
+
+    for i in 0..fch.len() {
+        if spec50[i] > spec24[i] {
+            fch[i] = clean_ch[i];
+        }
+    }
+    fch
 }
---
+++ src/zub_p.rs
@@ -0,0 +1,165 @@
+use crate::my_lib::{find_local_extrema, find_max, median_filter, my_filtfilt};
+use crate::time_param::TimeParam;
+use log::{debug, error};
+
+pub struct Zubp {
+    pub mean_amp_p: f32,
+    pub presence_pr: Vec<i32>,
+    pub intervals_pr: Vec<i32>,
+    pub inds_pr: Vec<usize>,
+}
+impl Zubp {
+    pub fn new(lead: &Vec<f32>, r_pos_len: usize) -> Zubp {
+        // let lead = lead;
+        Zubp {
+            mean_amp_p: 0.0,
+            presence_pr: vec![0; r_pos_len],
+            intervals_pr: vec![],
+            inds_pr: vec![],
+        }
+    }
+    pub fn get_mean_amp_pos(&mut self, lead: &Vec<f32>, time_param: &TimeParam) {
+        /*
+           Заполняет структуру Zubp
+        */
+        let lead = lead.clone();
+        let mut vec_amp_p: Vec<f32> = vec![];
+
+        for ind in &time_param.inds_min_diff {
+            let coef = 0.31 + time_param.intervals[*ind] / 2000.0;
+            let len_pr: i32 = (time_param.intervals[*ind] * coef) as i32; // *0.45
+            let start = (time_param.r_pos[*ind] - len_pr) as usize;
+            let stop = (time_param.r_pos[*ind] - 10) as usize;
+            if start > 20000000 {
+                // if start > time_param.r_pos[time_param.r_pos.len() - 1] as usize {
+                break;
+            }
+            let fragment = lead[start..stop].to_vec();
+            let (amp_p, ind_p) = self.get_amp_ind_p(&fragment);
+            let pr = len_pr - ind_p as i32;
+            if pr > 0 {
+                self.intervals_pr.push(pr);
+                self.inds_pr.push(*ind);
+            }
+            if (amp_p < 1.0) && (amp_p > 0.001) {
+                vec_amp_p.push(amp_p);
+            }
+        }
+        if vec_amp_p.is_empty() {
+            self.mean_amp_p = 0.0;
+        } else {
+            let sum: f32 = vec_amp_p.iter().sum();
+            let count = vec_amp_p.len() as f32;
+            self.mean_amp_p = sum / count;
+        }
+        self.intervals_pr = median_filter(&self.intervals_pr, 59); // 19
+        self.interp_pr(time_param);
+        self.clean_presence_pr();
+    }
+
+    fn get_amp_ind_p(&mut self, fragment: &Vec<f32>) -> (f32, usize) {
+        /*
+           Возвращает амп. P и его индекс в фрагменте(PR)
+           для вычисления mean_amp_p и mean_PR
+        */
+        let b: Vec<f32> = vec![0.02785977, 0.05571953, 0.02785977];
+        let a: Vec<f32> = vec![1.0, -1.47548044, 0.58691951];
+        let bi: Vec<f32> = vec![0.03634612, 0.03634612];
+        let ai: Vec<f32> = vec![1.0, -0.92730777];
+        let mut amp_p: f32 = 0.0;
+        let mut ind_p: usize = 0;
+        let mut fragment = my_filtfilt(&b, &a, &fragment);
+        let isoline = my_filtfilt(&bi, &ai, &fragment);
+        for i in 0..fragment.len() {
+            fragment[i] = fragment[i] - isoline[i];
+        }
+        let (vec_ind_extrema, vec_val_extrema) = find_local_extrema(&fragment);
+        if vec_val_extrema.len() >= 1 {
+            let (val_max_extrema, ind_max_extrema) = find_max(&vec_val_extrema);
+            ind_p = vec_ind_extrema[ind_max_extrema];
+            if ind_max_extrema == 0 {
+                amp_p = val_max_extrema;
+            } else if ind_max_extrema == vec_ind_extrema.len() - 1 {
+                amp_p = val_max_extrema;
+            } else {
+                let side1 = vec_val_extrema[ind_max_extrema] - vec_val_extrema[ind_max_extrema - 1];
+                let side2 = vec_val_extrema[ind_max_extrema] - vec_val_extrema[ind_max_extrema + 1];
+                if side1 < side2 {
+                    amp_p = side1;
+                } else {
+                    amp_p = side2;
+                }
+            }
+        }
+        (amp_p, ind_p)
+    }
+
+    pub fn get_p_in_lead(&mut self, lead: &Vec<f32>, time_param: &TimeParam) -> Vec<f32> {
+        let lead = lead.clone();
+        let mut p: Vec<f32> = vec![0.0; time_param.r_pos.len()];
+        let w = (self.mean_amp_p * 30.0) as i32 + 12; // +10
+        for i in 0..self.presence_pr.len() {
+            if time_param.r_pos[i] < lead.len() as i32 {
+                let start = (time_param.r_pos[i] - self.presence_pr[i] - w) as usize; // -15
+                let stop = if (self.presence_pr[i] - w) > 10 {
+                    (time_param.r_pos[i] - self.presence_pr[i] + w) as usize
+                } else {
+                    (time_param.r_pos[i] - 10) as usize
+                };
+                let fragment = lead[start..stop].to_vec();
+                if time_param.chars[i] != 'N' {
+                    p[i] = 1.0;
+                } else {
+                    let (amp_p, _ind_p) = self.get_amp_ind_p(&fragment);
+                    if amp_p > self.mean_amp_p * 0.2 {
+                        //0.12
+                        p[i] = 1.0;
+                    }
+                }
+            }
+        }
+        p
+    }
+
+    fn interp_line(&mut self, ind_start: usize, ind_stop: usize, val_start: i32, val_stop: i32) {
+        let diff_val = val_stop - val_start;
+        let count_step = ind_stop - ind_start;
+        if count_step > 1 {
+            let step_val = diff_val / count_step as i32;
+            for i in ind_start..ind_stop {
+                let j = i - ind_start;
+                self.presence_pr[i] = val_start + step_val * j as i32;
+            }
+        } else {
+            self.presence_pr[ind_start] = val_start;
+        }
+    }
+    fn interp_pr(&mut self, time_param: &TimeParam) {
+        for i in 1..self.inds_pr.len() - 1 {
+            let ind_start = self.inds_pr[i];
+            let ind_stop = self.inds_pr[i + 1];
+            let val_start = self.intervals_pr[i];
+            let val_stop = self.intervals_pr[i + 1];
+            self.interp_line(ind_start, ind_stop, val_start, val_stop);
+        }
+        self.interp_line(
+            0,
+            self.inds_pr[0],
+            self.intervals_pr[0],
+            self.intervals_pr[0],
+        );
+        self.interp_line(
+            self.inds_pr[self.inds_pr.len() - 1],
+            time_param.r_pos.len(),
+            self.intervals_pr[self.intervals_pr.len() - 1],
+            self.intervals_pr[self.intervals_pr.len() - 1],
+        );
+    }
+    fn clean_presence_pr(&mut self) {
+        for i in 1..self.presence_pr.len() - 1 {
+            if self.presence_pr[i] == 0 {
+                self.presence_pr[i] = self.presence_pr[i - 1];
+            }
+        }
+    }
+}
